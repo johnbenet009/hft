bb7c4c8c5bf1f298f9af213850761f7f16d7c289
Positive Developer <johnbenet0009@gmail.com>
2026-04-09 21:47:33 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,41 +1,34 @@
 const CACHE_NAME = 'hft-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/logo.svg',
-  '/manifest.json'
+  // Use relative paths so this works when hosted on a subpath (e.g. /hft/ on GitHub Pages)
+  './',
+  './index.html',
+  './logo.svg',
+  './manifest.json',
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request);
+    })
   );
 });
 
 self.addEventListener('activate', event => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
+    caches.keys().then(cacheNames =>
+      Promise.all(
         cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+          if (!cacheWhitelist.includes(cacheName)) return caches.delete(cacheName);
         })
-      );
-    })
+      )
+    )
   );
 });
